0b7103b50839f88aae99ad5dd3690c5b4749b17d
TureLindbladDesktop <ture.gustaf.lindblad@gmail.com>
2024-09-27 08:17:35 +0200

    Add search suggestions

--- StonkRocket.Client/StonkRocket/src/components/SearchComponent.jsx
+++ StonkRocket.Client/StonkRocket/src/components/SearchComponent.jsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from 'react-router-dom';
+import config from "../config"
+import "../styling/Searchbar.css"
 
-const SearchComponent = () =>{
+const SearchComponent = () => {
     const [input, setInput] = useState('');
+    const [tickers, setTickers] = useState([]);
+    const [suggestions, setSuggestions] = useState([]);
+    const [viewSuggestions, setViewSuggestions] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch(`${config.stonkRocketApiUrl}/stocks`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Error getting stocks: ${response.status}`)
+                }
+                return response.json()
+            })
+            .then(data => {
+                const mapedTickers = data.stocks.map(stock => stock.ticker)
+                setTickers(mapedTickers)
+                setSuggestions(mapedTickers)
+            })
+            .catch(error => console.log('Error loading data', error))
+    }, [])
+
+    useEffect(() => {
+        if (input) {
+            setViewSuggestions(true)
+        } else {
+            setViewSuggestions(false)
+        }
+
+        const filter = tickers.filter(ticker => ticker.toLowerCase().includes(input.toLowerCase()))
+        setSuggestions(filter)
+    }, [input])
 
     const handleInput = (e) =>{
         setInput(e.target.value)
@@ -16,13 +48,23 @@ const SearchComponent = () =>{
     return(
         <form onSubmit={handleSubmit}>
             <input 
-            type="text"
-            value={input}
-            onChange={handleInput}
-            placeholder=" Ange Ticker"
+                type="text"
+                value={input}
+                onChange={handleInput}
+                placeholder="search ticker..."
             />
-            <button type="submit" >Sök</button>
 
+            {viewSuggestions && suggestions.length > 0 && 
+                <div className="suggestions">
+                    {suggestions.map((suggestion) => (
+                        <Link key={suggestion} to={`/stockviewpage?search=${suggestion}`}>
+                            {suggestion}
+                        </Link>
+                    ))}
+                </div>
+            }
+
+            <button type="submit">Search</button>
         </form>
     )
 }
