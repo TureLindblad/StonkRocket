aae53a585d50ec015dbb8b8561f052fadce584fb
kevinmartiin <nextgenallu@hotmail.com>
2024-10-01 15:15:57 +0200

    Added some documentation to swagger endpoints

--- StonkRocket.API/StonkRocket.API/Endpoints.cs
+++ StonkRocket.API/StonkRocket.API/Endpoints.cs
@@ -14,25 +14,37 @@ namespace StonkRocket.API
         private static void MapStockEndpoints(IEndpointRouteBuilder app)
         {
             app.MapGet("/stocks", (IUsersService usersService)
-                => usersService.GetStocks());
+                => usersService.GetStocks())
+                .WithDescription("Retrieves all stocks listed in database")
+                .WithTags("Stocks");
 
             app.MapPost("/stock/{ticker}", (IUsersService userService, string ticker)
-                => userService.PostStock(ticker));
+                => userService.PostStock(ticker))
+                .WithDescription("Posts stocks using ticker identifier to database")
+                .WithTags("Stocks");
         }
 
         private static void MapUserEndpoints(IEndpointRouteBuilder app)
         {
-            app.MapGet("/user/{id}", (IUsersService usersService, int id)
-                => usersService.GetUserByID(id));
+            app.MapGet("/user/{userId}", (IUsersService usersService, int userId)
+                => usersService.GetUserByID(userId))
+                .WithDescription("Retrieves users by ID from database")
+                .WithTags("Users");
 
             app.MapGet("/user/validate/{username}", (IUsersService usersService, string username)
-                => usersService.ValidateUser(username));
+                => usersService.ValidateUser(username))
+                .WithDescription("Compare username sent by URL to usernames in database and send back userID")
+                .WithTags("Users");
 
-            app.MapPost("/user/stocks/{id}", (IUsersService userService, int id, [FromQuery] string ticker)
-                => userService.PostUserStock(ticker, id));
+            app.MapPost("/user/stocks/{userId}", (IUsersService userService, int userId, [FromQuery] string ticker)
+                => userService.PostUserStock(ticker, userId))
+                .WithDescription("Posts stock using ticker identifier to user stock table in database. Matches ticker with userID")
+                .WithTags("Users");
 
-            app.MapDelete("/user/stocks/{id}", (IUsersService userService, int id, [FromQuery] string ticker)
-                => userService.DeleteUserStock(ticker, id));
+            app.MapDelete("/user/stocks/{userId}", (IUsersService userService, int userId, [FromQuery] string ticker)
+                => userService.DeleteUserStock(ticker, userId))
+                .WithDescription("Delete stock from user stock table in database. Matches user id to specified ticker and deletes post in database")
+                .WithTags("Users");
         }
     }
 }
